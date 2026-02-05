f86bb541160ad72ddf595b9869b2aa6b19b781a7
Vicentte Matheus <vicentte-matheus@hotmail.com>
2026-02-05 01:24:16 -0300

    Templates modificados para exibir postagens do banco de dados.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
+'''def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {})'''
+
+'''def post_list(request):
+    return render(request, 'blog/post_list.html', {
+        'posts': ['post 1', 'post 2', 'post 3']
+    })
+'''
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
